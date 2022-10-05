5e4ea9a8b0644e374520b7a47fb209507ad3aaeb
Caelan <caelan.curry@gmail.com>
2022-10-05 09:29:45 +0200

    Header setup

---
+++ components/Header/Header.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { AiFillGithub, AiFillInstagram, AiFillLinkdin } from "react-icons/ai";
+import { DiCssdeck } from "react-icons/di";
+
+//Styled comps
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from "./HeaderStyles";
+
+const Header = () => {
+  return (
+    <Container>
+      <Div1>
+        <Link href="/">
+          <a style={{ display: "flex", alignItems: "center", color: "white" }}>
+            <DiCssdeck size="3rem" />
+            <span>Portfolio</span>
+          </a>
+        </Link>
+      </Div1>
+    </Container>
+  );
+};
+
+export default Header;

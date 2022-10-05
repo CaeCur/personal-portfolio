a212e6b226f611eb1ed9de7530207ab8e223182d
Caelan <caelan.curry@gmail.com>
2022-10-05 09:49:50 +0200

    Init commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,8 @@ import Theme from "../styles/theme";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Theme>
-        <Component {...pageProps} />
-      </Theme>
-    </>
+    <Theme>
+      <Component {...pageProps} />
+    </Theme>
   );
 }

7b4ff83e70e8ae9168f8fda1d047ac4ea1f8701e
Caelan <caelan.curry@gmail.com>
2022-10-05 09:50:22 +0200

    Added Header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
         <Link href="/">
           <a style={{ display: "flex", alignItems: "center", color: "white" }}>
             <DiCssdeck size="3rem" />
-            <span>Portfolio</span>
+            <span>Caelan Curry</span>
           </a>
         </Link>
       </Div1>

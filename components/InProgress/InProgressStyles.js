a3f619b45313f14f0deede35facfa66824ff4415
Caelan <caelan.curry@gmail.com>
2022-10-08 14:02:05 +0200

    Better card placement

--- components/InProgress/InProgressStyles.js
+++ components/InProgress/InProgressStyles.js
@@ -4,7 +4,7 @@ export const GridContainer = styled.section`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
   padding: 3rem;
-  place-items: center;
+  align-items: stretch;
   column-gap: 2rem;
   row-gap: 3rem;
   @media ${(props) => props.theme.breakpoints.sm} {
